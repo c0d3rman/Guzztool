70b5a8dfc6eabb26dfe0d97f558518f8a5edd834
Yoni Lerner <yonilern@gmail.com>
2024-05-05 18:28:27 -0700

    Changed to no-clone animation

--- src/options/options.target.js
+++ src/options/options.target.js
@@ -1,6 +1,6 @@
-import { browser } from '@guzztool/util/util.js';
-import log from '@guzztool/util/log.js';
-import stickerify from '@guzztool/util/stickerify.js';
+import { browser } from '@guzztool/util/util';
+import log from '@guzztool/util/log';
+import stickerify from '@guzztool/util/stickerify';
 import subpageTemplate from './subpage.handlebars';
 
 
@@ -8,20 +8,59 @@ import subpageTemplate from './subpage.handlebars';
 
 
 $(function () {
-    // Set size of toggle switches based on their parent cells
-    const stylesheet = Array.from(document.styleSheets)
-        .find(sheet => sheet.ownerNode.id === "toggle_switch_css");
-    const rule = Array.from(stylesheet.cssRules)
-        .find(cssRule => cssRule.selectorText === '.toggle-switch');
-    const resize = _ => {
-        rule.style.setProperty("--size", $('.cell').width() / 10 + 'px');
+    /**
+     * Numericizes all given properties of the element.
+     * @param {*} el 
+     */
+    $.fn.numericize = function (props = null, inplace = true) {
+        if (props === null) {
+            // These are all the properties that affect an element's sizing
+            props = ["width", "height",
+                "padding-top", "padding-bottom", "padding-left", "padding-right",
+                "margin-top", "margin-bottom", "margin-left", "margin-right",
+                "border-top-width", "border-bottom-width", "border-left-width", "border-right-width", "border-radius"];
+        }
+        const getOriginal = (el) => Object.fromEntries(props.map(prop => [prop, $(el).prop('style')[prop]]));
+        const getNumericized = (el) => Object.fromEntries(props.map(prop => [prop, $(el).css(prop)]));
+        if (inplace) {
+            this.each(function () {
+                // Save the current values of the CSS properties we'll be modifying,
+                // specifically any set on the "style" of the element, so we can undo the change at the end
+                $(this).data('_numericize_original_props',
+                    Object.assign($(this).data('_numericize_original_props') ?? {}, getOriginal(this)));
 
-        // Fit titles to their cells
+                // Numericize the properties
+                $(this).css(getNumericized(this));
+            });
+            return this;
+        } else {
+            // Return the numericized properties and originals
+            return this.map(function () {
+                return [getNumericized(this), getOriginal(this)];
+            }).get();
+        }
+    }
+
+    $.fn.unnumericize = function () {
+        this.each(function () {
+            $(this).css($(this).data('_numericize_original_props'));
+            $(this).removeData('_numericize_original_props');
+        });
+        return this;
+    }
+
+
+
+    const resize = () => {
+        // Set size of toggle switches based on their parent cells (except in subpage)
+        $(".cell:not(.subpage) .toggle-switch").css("--size", $('.cell').width() / 10 + 'px');
+
+        // Fit titles to their cells (except in subpage)
         const percentWidth = (el) => $(el).width() / $(el).parent().width();
         const maxPercentWidthName = $('.subtool-name').toArray().reduce((max, el) => percentWidth(el) > percentWidth(max) ? el : max);
         const adjustmentFactor = 1 / percentWidth(maxPercentWidthName);
         const fontSize = parseFloat(window.getComputedStyle(maxPercentWidthName, null).getPropertyValue('font-size'));
-        $('.subtool-name').css('font-size', `${adjustmentFactor * fontSize}px`);
+        $('.cell:not(.subpage) .subtool-name').css('font-size', `${adjustmentFactor * fontSize}px`);
     }
     $(window).on('resize', resize);
     resize();
@@ -39,12 +78,13 @@ $(function () {
     });
 
     // Listen for changes in the toggle switches and update the storage
-    $('.toggle-switch input').change(function () {
+    const enableSwitchListener = function () {
         browser.storage.sync.get('options', data => {
             data.options[$(this).closest('.cell').data('subtool-id')]['enabled'] = $(this).prop('checked');
             browser.storage.sync.set({ options: data.options });
         });
-    });
+    };
+    $('.toggle-switch input').change(enableSwitchListener);
 
     // Turn all subtool icons into stickers
     $('.cell .subtool-icon').each(function () { stickerify(this) });
@@ -53,66 +93,41 @@ $(function () {
     $('.cell').click(function (event) {
         if ($('#app').find('.subpage').length > 0) return; // If a subpage is already open, don't open another
 
-        const cell = this;
-
-        // These are all the properties that affect an element's sizing
-        const sizeProperties = ["width", "height",
-            "padding-top", "padding-bottom", "padding-left", "padding-right",
-            "margin-top", "margin-bottom", "margin-left", "margin-right",
-            "border-top-width", "border-bottom-width", "border-left-width", "border-right-width", "border-radius"];
-
-        // Save the current values of the CSS properties we'll be modifying, specifically any set on the "style" of the element, so we can undo the change at the end
-        const originalCSS = sizeProperties.reduce((acc, cssProp) => {
-            acc[cssProp] = $(this).prop('style')[cssProp];
-            return acc;
-        }, {});
-
-        // Clone the cell into #app, copy its numericized size properties, and place the clone on top of it with absolute positioning
-        const clone = $(this).clone().appendTo('#app').css(Object.assign({
+        // Numericize the cell's size and position as well as some of its components
+        $(this).find('.subtool-icon').numericize(["width", "height"]);
+        $(this).find('.settings-icon').numericize().numericize(["top", "right"]); // Fix the size and position of the settings icon, since it growing rapidly looks weird
+        $(this).find('.cell-content').numericize(["width", "height"]); // Keep the .cell-content the same size as any other cell
+        $(this).numericize().css({
             'position': 'absolute',
             'top': $(this).offset().top,
             'left': $(this).offset().left,
-        }, sizeProperties.reduce((acc, cssProp) => {
-            acc[cssProp] = $(this).css(cssProp);
-            return acc;
-        }, {})));
+        });
 
-        // Clone the canvas data
-        const oldCanvas = $(this).find('canvas')[0];
-        const newCanvas = clone.find('canvas')[0];
-        newCanvas.width = oldCanvas.width;
-        newCanvas.height = oldCanvas.height;
-        newCanvas.getContext('2d').drawImage(oldCanvas, 0, 0);
+        // Move the cell out of the grid and replace with a placeholder
+        const placeholder = $('<div class="cell placeholder"></div>').insertAfter(this);
+        $(this).appendTo('#app');
 
-        // Clone the current opacity of the hover overlay
-        clone.find('.hover-overlay').removeClass('hover-animation-active')
-            .css('opacity', $(this).find('.hover-overlay').css('opacity'));
-
-        // Fix the size and position of the settings icon in the clone, since it growing rapidly looks weird
-        clone.find('.settings-icon').css(sizeProperties.concat(['top', 'right']).reduce((acc, cssProp) => {
-            acc[cssProp] = clone.find('.settings-icon').css(cssProp);
-            return acc;
-        }, {}));
-
-        // Perform the animation to fullscreen the clone
-        clone.addClass('subpage');
+        // Perform the animation to fullscreen the cell
+        $(this).addClass('subpage');
+        $(this).removeClass("cell-shadow");
         const animationDuration = parseInt(getComputedStyle($("#grid").get()[0], null).getPropertyValue('--subpage-animation-duration')) * 1000; // Get animation duration from CSS
-        clone.find('.hover-overlay').animate({ opacity: 0 }, { duration: animationDuration, queue: false });
+        $(this).find('.hover-overlay').animate({ opacity: 0 }, { duration: animationDuration, queue: false });
         setTimeout(() => {
-            clone.css({ top: '', left: '' }); // We need to unset these since they take priority over the class
-            // Restore the element's original size-related CSS properties onto the clone
-            clone.css(Object.assign({}, originalCSS));
+            $(this).unnumericize().css({
+                'top': '',
+                'left': '',
+            });
         }, 1); // This needs to happen a bit after the class change, otherwise the CSS doesn't animate
 
         browser.storage.sync.get('options', data => {
             // Create the subpage
             const subtoolId = $(this).data('subtool-id');
-            clone.append(subpageTemplate(SUBTOOLS[subtoolId]));
+            $(this).append(subpageTemplate(SUBTOOLS[subtoolId]));
 
             // Load current settings values
             const currentSettings = data.options[subtoolId].subtool_settings;
             Object.entries(currentSettings).forEach(([id, value]) => {
-                const input = clone.find(`input[name="${id}"]`);
+                const input = $(this).find(`input[name="${id}"]`);
                 if (input.attr('type') === 'checkbox') {
                     input.prop('checked', value);
                 } else {
@@ -121,7 +136,7 @@ $(function () {
             });
 
             // Link input states to their corresponding settings
-            clone.find('input').change(function () {
+            $(this).find('input').change(function () {
                 browser.storage.sync.get('options', data => {
                     const value = $(this).prop("type") === "checkbox" ? $(this).prop("checked") : $(this).val();
                     data.options[subtoolId]['subtool_settings'][$(this).attr('name')] = value;
@@ -130,18 +145,25 @@ $(function () {
             });
 
             // Add a click handler for the back button
-            clone.find('.back-button').click(function () {
+            $(this).find('.back-button').click(() => {
                 // Animate subpage close
-                clone.find(".subpage-content").fadeOut(animationDuration);
-                clone.css(Object.assign({
+                $(this).find(".subpage-content").fadeOut(animationDuration);
+                $(this).addClass("cell-shadow");
+                const [numericized, original] = $(placeholder).numericize(null, false);
+                $(this).css(Object.assign({
                     'position': 'absolute',
-                    'top': $(cell).offset().top,
-                    'left': $(cell).offset().left,
-                }, sizeProperties.reduce((acc, cssProp) => {
-                    acc[cssProp] = $(cell).css(cssProp);
-                    return acc;
-                }, {})));
-                setTimeout(() => clone.remove(), animationDuration);
+                    'top': placeholder.offset().top,
+                    'left': placeholder.offset().left,
+                }, numericized));
+                setTimeout(() => {
+                    $(this).removeClass('subpage').insertAfter(placeholder);
+                    $(this).find(".subpage-content").remove();
+                    placeholder.remove();
+                    $(this).find('.subtool-icon').unnumericize();
+                    $(this).find('.settings-icon').unnumericize();
+                    $(this).find('.cell-content').unnumericize();
+                    $(this).css(Object.assign({ position: '', top: '', left: '' }, original));
+                }, animationDuration);
             });
         });
     }).on('click', '.toggle-switch', (e) => e.stopPropagation()); // Don't treat a click on the toggle switch as a click on the parent cell
