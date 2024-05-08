8758e8eb4571aef096958083e88fafc150d8b1e9
Yoni Lerner <yonilern@gmail.com>
2024-05-08 02:49:42 -0700

    Linter fix

--- WebpackAddonLinterPlugin.mjs
+++ WebpackAddonLinterPlugin.mjs
@@ -3,20 +3,19 @@ import addonsLinter from 'addons-linter';
 class WebpackAddonLinterPlugin {
     constructor(options) {
         this.options = options;
-        this.linter = addonsLinter.createInstance({
-            config: {
-                _: [this.options.path],
-                logLevel: 'debug',
-                stack: true,
-                shouldScanFile: fileName => fileName != 'static/lib/jquery.min.js',
-            },
-            runAsBinary: false, // This stops the linter from exiting the nodejs application
-        });
     }
 
     apply(compiler) {
         compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-            this.linter.run();
+            addonsLinter.createInstance({
+                config: {
+                    _: [this.options.path],
+                    logLevel: 'fatal',
+                    stack: true,
+                    shouldScanFile: fileName => fileName != 'static/lib/jquery.min.js',
+                },
+                runAsBinary: false, // This stops the linter from exiting the nodejs application
+            }).run();
         });
     }
 };
