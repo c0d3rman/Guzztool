9af1135ba2a157781f928aa7daa58e693826d59c
Yoni Lerner <yonilern@gmail.com>
2024-06-28 21:22:43 -0700

    Made BGM adaptable

--- src/subtools/primarinas_jukebox/inject.js
+++ src/subtools/primarinas_jukebox/inject.js
@@ -2,8 +2,70 @@ const subtool = {
 	init: function () {
 		if (!this.roomListener) return;
 
+		// TBD: get music settings from content script
+		this.bgmSettings = {
+			"normal": [
+				{ // Default
+					bgmUrl: "https://play.pokemonshowdown.com/audio/sm-trainer.mp3",
+					bgmStart: 8323,
+					bgmEnd: 89230,
+				},
+				{
+					bgmUrl: 'https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/xy-elite4.mp3',
+					bgmStart: 133673,
+					bgmEnd: 261675,
+				},
+				{
+					bgmUrl: 'https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bw-n-final.mp3',
+					bgmStart: 42532,
+					bgmEnd: 129714,
+				},
+				{
+					bgmUrl: 'https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bdsp-giratina.mp3',
+					bgmStart: 60527,
+					bgmEnd: 164162,
+				},
+				{
+					bgmUrl: 'https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/b2w2-plasma.mp3',
+					bgmStart: 264410,
+					bgmEnd: 438982,
+				},
+				{
+					bgmUrl: 'https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bdsp-galactic-admin.mp3',
+					bgmStart: 119450,
+					bgmEnd: 176991,
+				},
+			],
+			"special": {
+				"dogars": {
+					// If your mon is a Koffing named "dogars"
+					bgmUrl: 'https://play.pokemonshowdown.com/audio/bw2-homika-dogars.mp3',
+					bgmStart: 1661,
+					bgmEnd: 68131,
+				},
+				"ludicolo-lombre": {
+					// If there's at least two mons from the Ludicolo line in team preview
+					bgmUrl: 'https://play.pokemonshowdown.com/audio/xd-miror-b.mp3',
+					bgmStart: 9000,
+					bgmEnd: 57815,
+				},
+				"ludicolo": {
+					// If there's at least two Ludicolos in team preview
+					bgmUrl: 'https://play.pokemonshowdown.com/audio/colosseum-miror-b.mp3',
+					bgmStart: 896,
+					bgmEnd: 47462,
+				},
+				"spl": {
+					// If you're in an SPL game
+					bgmUrl: "https://play.pokemonshowdown.com/audio/spl-elite4.mp3",
+					bgmStart: 3962,
+					bgmEnd: 152509,
+				},
+			}
+		};
+
 		this.intercept(window.BattleScene, "setBgm", this.setBgm);
-		// this.intercept(window.BattleScene, "rollBgm", this.rollBgm);
+		this.intercept(window.BattleScene, "rollBgm", this.rollBgm);
 		this.intercept(window.BattleSound, "getSound", this.getSound);
 	},
 
@@ -25,41 +87,24 @@ const subtool = {
 		try {
 			if (this.bgmNum === bgmNum) return;
 			this.bgmNum = bgmNum;
+			self.log.info(`Setting bgm to ${bgmNum}`);
 
-			switch (bgmNum) {
-				case -1:
-					this.bgm = BattleSound.loadBgm('https://play.pokemonshowdown.com/audio/bw2-homika-dogars.mp3', 1661, 68131, this.bgm);
-					break;
-				case -2:
-					this.bgm = BattleSound.loadBgm('https://play.pokemonshowdown.com/audio/xd-miror-b.mp3', 9000, 57815, this.bgm);
-					break;
-				case -3:
-					this.bgm = BattleSound.loadBgm('https://play.pokemonshowdown.com/audio/colosseum-miror-b.mp3', 896, 47462, this.bgm);
-					break;
-				case -101:
-					this.bgm = BattleSound.loadBgm('https://play.pokemonshowdown.com/audio/spl-elite4.mp3', 3962, 152509, this.bgm);
-					break;
+			const load = (bgm) => { this.bgm = BattleSound.loadBgm(bgm.bgmUrl, bgm.bgmStart, bgm.bgmEnd, this.bgm) };
 
-				case 1:
-					this.bgm = BattleSound.loadBgm('https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/xy-elite4.mp3', 133673, 261675, this.bgm);
-					break;
-				case 2:
-					this.bgm = BattleSound.loadBgm('https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bw-n-final.mp3', 42532, 129714, this.bgm);
-					break;
-				case 3:
-					this.bgm = BattleSound.loadBgm('https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bdsp-giratina.mp3', 60527, 164162, this.bgm);
-					break;
-				case 4:
-					this.bgm = BattleSound.loadBgm('https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/b2w2-plasma.mp3', 264410, 438982, this.bgm);
-					break;
-				case 5:
-					this.bgm = BattleSound.loadBgm('https://github.com/OpenSauce04/ssmm-showdown/raw/master/music/bdsp-galactic-admin.mp3', 119450, 176991, this.bgm);
-					break;
-
-				case 6:
-				default:
-					this.bgm = BattleSound.loadBgm('https://play.pokemonshowdown.com/audio/sm-trainer.mp3', 8323, 89230, this.bgm);
-					break;
+			if (bgmNum == -1 && "dogars" in self.bgmSettings.special) {
+				load(self.bgmSettings.special["dogars"]);
+			} else if (bgmNum == -2 && "ludicolo-lombre" in self.bgmSettings.special) {
+				load(self.bgmSettings.special["ludicolo-lombre"]);
+			} else if (bgmNum == -3 && "ludicolo" in self.bgmSettings.special) {
+				load(self.bgmSettings.special["ludicolo"]);
+			} else if (bgmNum == -101 && "spl" in self.bgmSettings.special) {
+				load(self.bgmSettings.special["spl"]);
+			}
+			else if (bgmNum > 0 && bgmNum < self.bgmSettings.normal.length) {
+				load(self.bgmSettings.normal[bgmNum - 1]);
+			}
+			else {
+				load(self.bgmSettings.normal[0]);
 			}
 
 			this.updateBgm();
@@ -70,7 +115,7 @@ const subtool = {
 	},
 
 	rollBgm(self, originalMethod) {
-		this.setBgm(1 + this.numericId % 2);
+		this.setBgm(1 + this.numericId % self.bgmSettings.normal.length);
 	},
 
 	getSound(self, originalMethod, url) {
