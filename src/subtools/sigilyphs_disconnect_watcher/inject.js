af11657e1d1d955ee05aedc7ba795a07eb1ed6ee
Yoni Lerner <yonilern@gmail.com>
2024-08-03 22:36:36 -0700

    Modified sigilyph's to use existing chatter

--- src/subtools/sigilyphs_disconnect_watcher/inject.js
+++ src/subtools/sigilyphs_disconnect_watcher/inject.js
@@ -2,14 +2,21 @@ import { FunctionListenerProxy } from '@guzztool/util/ListenerProxy.js';
 
 const subtool = {
     init: function () {
+        // Intercept all incoming messages to check if we are alive
+        this.receiveProxy = new FunctionListenerProxy(app.receive, (originalFn, ...args) => {
+            try {
+                this.registerPong();
+                originalFn(...args);
+            } catch (e) { this.log.error(e) };
+        });
+        app.receive = this.receiveProxy.proxy;
+
         app.on("loggedin", () => {
             try {
                 this.log.info(`Login detected, setting up disconnect watcher.`);
-                clearInterval(this.interval); // In case this is our second login
+                this.listenForPong();
 
-                this.receivedPong = true; // Set to false when sending a ping and back to true when receiving a pong
-
-                // Intercept incoming PMs to receive pongs (and prevent them from displaying)
+                // Intercept incoming PMs to prevent pongs from displaying
                 this.addPMProxy = new FunctionListenerProxy(app.rooms[""].addPM, (originalFn, ...args) => {
                     try {
                         const sender = toID(args[0]);
@@ -18,8 +25,8 @@ const subtool = {
                         const message = args[1];
 
                         if (sender == me && receiver == me && message == "GUZZTOOL_SIGILYPH_PING") {
-                            this.receivedPong = true;
                             this.log.debug("Received pong.");
+                            // Pong will be registered by update()
                             return;
                         }
 
@@ -28,32 +35,42 @@ const subtool = {
                 });
                 app.rooms[""].addPM = this.addPMProxy.proxy;
 
-                // Send a ping every so often (and check if we received a pong since the last one)
-                this.interval = setInterval(() => {
-                    try {
-                        if (!this.receivedPong) {
-                            this.log.warn("Disconnected from server.");
-                            app.addPopupMessage("Sigilyph says: Disconnected from server. Please refresh the page.");
-                            clearInterval(this.interval);
-                        } else {
-                            this.receivedPong = false;
-                            app.rooms[''].send(`/pm ${app.user.get("userid")}, GUZZTOOL_SIGILYPH_PING`);
-                            this.log.debug("Sent ping.");
-                        }
-                    } catch (e) { this.log.error(e) };
-                }, this.options.pollingInterval * 1000);
-
                 // Intercept logouts so we can stop the disconnect watcher
                 this.logoutProxy = new FunctionListenerProxy(app.user.logout, (originalFn, ...args) => {
                     try {
                         this.log.debug("Logout detected, stopping disconnect watcher.");
-                        clearInterval(this.interval);
+                        clearTimeout(this.timeout);
                         originalFn(...args);
                     } catch (e) { this.log.error(e) };
                 });
                 app.user.logout = this.logoutProxy.proxy;
             } catch (e) { this.log.error(e) };
         }, this);
+    },
+
+    registerPong: function () {
+        this.pingSent = false;
+        this.listenForPong();
+    },
+
+    listenForPong: function (timeout = null) {
+        timeout = timeout || this.options.pollingInterval * 1000;
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+            try {
+                if (!this.pingSent) {
+                    // If we haven't had any contact with the server in a while, send a ping
+                    app.rooms[''].send(`/pm ${app.user.get("userid")}, GUZZTOOL_SIGILYPH_PING`);
+                    this.pingSent = true;
+                    this.log.debug("Sent ping.");
+                    this.listenForPong(this.options.timeoutDuration * 1000);
+                } else {
+                    // If you sent a ping already and didn't get a pong, you are disconnected
+                    this.log.warn("Disconnected from server.");
+                    app.addPopupMessage("Sigilyph says: Disconnected from server. Please refresh the page.");
+                }
+            } catch (e) { this.log.error(e) };
+        }, timeout);
     }
 }
 
